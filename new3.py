4badb0906de24e732cb4d77fb2b8277cb69b5267
tharlinnhtet <tharlinnhtet44@gmail.com>
2025-11-30 11:52:22 +0630

    Python2

---
+++ new3.py
@@ -0,0 +1,13 @@
+num1 = input("Enter Number1\n")
+num2 = input("Enter Number2\n")
+Add = int(num1) + int(num2)
+Sub = int(num1) - int(num2)
+Multi = int(num1) * int(num2)
+Div = int(num1) / int(num2)
+
+
+
+print(f"Your Addition answer is {Add}")
+print(f"Your Substarction answer is {Sub}")
+print(f"Your Multiplication answer is {Multi}")
+print(f"Your Division answer is {Div}")
